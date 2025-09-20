66245babeb3f3a4f839fb31e2be9f28c00f632d5
Gabriel Hamasaki <gabriel.hamasaki@exata.it>
2025-09-20 14:45:59 -0300

    feat: add compare hashed password

--- services/identity/utils/utils.go
+++ services/identity/utils/utils.go
@@ -13,3 +13,7 @@ func Bcrypt(password string) (string, error) {
 
 	return string(hashedPassword), nil
 }
+
+func CompareHashAndPassword(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
